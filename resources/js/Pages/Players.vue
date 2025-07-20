0f236a27b480a51ba00cd3b4bdce0531980ac095
Mehta Jay <dev.mehtajay@gmail.com>
2025-07-21 00:42:58 +0530

    removed vuedraggable package

--- resources/js/Pages/Players.vue
+++ resources/js/Pages/Players.vue
@@ -22,23 +22,6 @@ onMounted(async () => {
   getData()
 })
 
-const savePlayers = async () => {
-  const payload = {
-    teams: teams.value.map(team => ({
-      team_id: team.id,
-      players: team.players.map((player, index) => ({
-        player_id: player.id,
-        sort_order: index
-      }))
-    })),
-    unassigned: unassignedPlayers.value.map(p => p.id)
-  }
-
-  await axios.post('/api/players', payload)
-  alert('Teams saved!')
-  getData()
-}
-
 const headersList = [
   { title: 'ID', key: 'id', align: 'start' },
   { title: 'Name', key: 'name', align: 'center' },
@@ -81,45 +64,6 @@ const headersList = [
             </div>
           </template>
         </v-data-table>
-
-        <!-- Unassigned -->
-        <div class="mb-6">
-          <h3 class="text-lg font-semibold mb-2">Unassigned Players</h3>
-          <draggable v-model="unassignedPlayers" group="players" item-key="id" class="flex flex-wrap gap-4">
-            <template #item="{ element }">
-              <div class="p-3 w-50 bg-white rounded shadow border border-black">
-                <p><strong>{{ element.name }}</strong></p>
-                <p class="text-sm">{{ element.email }}</p>
-                <p class="text-xs">{{ element.date_of_birth }}</p>
-              </div>
-            </template>
-          </draggable>
-        </div>
-
-        <!-- Teams -->
-        <h3 class="text-lg font-semibold mb-2">Teams</h3>
-        <div class="grid grid-cols-1 md:grid-cols-2 gap-6">
-          <div v-for="team in teams" :key="team.id" class="p-4 border bg-blue-50 rounded">
-            <h4 class="text-lg font-bold">{{ team.name }}</h4>
-            <p class="text-sm text-gray-600">{{ team.state }}, {{ team.country }}</p>
-
-            <draggable v-model="team.players" group="players" item-key="id"
-              class="min-h-[100px] mt-3 p-2 bg-white border rounded">
-              <template #item="{ element }">
-                <div class="p-2 bg-gray-100 border rounded mb-2">
-                  <p><strong>{{ element.name }}</strong></p>
-                </div>
-              </template>
-            </draggable>
-          </div>
-        </div>
-
-        <div class="text-center">
-          <button class="text-center mt-6 bg-green-600 text-white px-4 py-2 rounded hover:bg-green-700"
-            @click="savePlayers">
-            Save
-          </button>
-        </div>
 
       </v-card>
     </div>
